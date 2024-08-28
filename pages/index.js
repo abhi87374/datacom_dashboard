ed4f1a15bdb2707d91a246004db03f0cff496315
Abhikumar Gupta <abhi873gupta@gmail.com>
2024-08-28 22:06:22 +0530

    initial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import PageBody from "../components/pagebody/PageBody";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    
+      <PageBody />
+    
+  );
+}
